c70100c39dd119aa0bac9761cafd694fa0c72f4d
Bipllab-Roy <bipllabroy.infinite>
2025-09-17 01:51:46 +0600

    first comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
-const App = () => {
-  return (
-    <div className='text-red-500'>
-      Home
-    </div>
-  )
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children:[
+      { index: true, Component: Home},
+      { path: "/about", Component: About},
+      { path: "*", Component: NotFound},
+  ]
+  }
+])
+
+const App = () => <RouterProvider router={router}/>  
 
 export default App
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+
+import { Outlet } from 'react-router'
+import Navbar from '../components/Navbar'
+const MainLayout = () => {
+  return (
+    <main>
+      <Navbar />
+        <Outlet />
+    </main>
+  )
+}
+
+export default MainLayout
